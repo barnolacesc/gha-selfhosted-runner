fdfc347ae8014e54b71f88d55090629461831d22
barnolacesc <francescbarnolacalls@gmail.com>
2025-06-07 13:10:14 +0200

    Make runner more resilient

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,21 @@ remove_runner() {
     ./config.sh remove --token "${RUNNER_TOKEN}"
 }
 
+# Function to start the runner
+start_runner() {
+    echo "Starting runner..."
+    ./run.sh
+}
+
 # Register the runner
 ./config.sh --url "${REPO_URL}" --token "${RUNNER_TOKEN}" --name "${RUNNER_NAME}" --unattended
 
 # Set up cleanup on container exit
 trap remove_runner EXIT
 
-# Start the runner
-./run.sh 
+# Start the runner and keep it running
+while true; do
+    start_runner
+    echo "Runner stopped. Restarting in 5 seconds..."
+    sleep 5
+done 
